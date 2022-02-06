16852adf4588af13dd325a093f60de1dfce9390c
Norbert Wojtowicz <norbi2790@gmail.com>
2022-02-06 20:14:06 +0100

    Implement the API structure of the service

---
+++ dartsgame/controller/GameController.java
@@ -0,0 +1,49 @@
+package dartsgame.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+@RestController
+@RequestMapping("api/game")
+public class GameController {
+    @PostMapping("/create")
+    public Map<String, String> createGame() {
+        return new ConcurrentHashMap<>(
+                Map.of("status", "Under construction!")
+        );
+    }
+
+    @GetMapping("/list")
+    public Map<String, String> getListOfGames() {
+        return new ConcurrentHashMap<>(
+                Map.of("status", "Under construction!")
+        );
+    }
+
+    @GetMapping("/join")
+    public Map<String, String> joinGame() {
+        return new ConcurrentHashMap<>(
+                Map.of("status", "Under construction!")
+        );
+    }
+
+    @GetMapping("/status")
+    public Map<String, String> getGameStatus() {
+        return new ConcurrentHashMap<>(
+                Map.of("status", "Under construction!")
+        );
+    }
+
+    @PostMapping("/throws")
+    public Map<String, String> throwDart() {
+        return new ConcurrentHashMap<>(
+                Map.of("status", "Under construction!")
+        );
+    }
+}
